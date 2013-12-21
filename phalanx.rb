e780736176f5ccf833d6d6744c15584b83b50861
Abinoam Jr <abinoam@gmail.com>
2013-12-20 21:19:18 -0300

    Use method instead of instance variables

--- phalanx.rb
+++ phalanx.rb
@@ -30,39 +30,43 @@ class Game < Gosu::Window
     true
   end
 
+  def current_state
+    @states[@current_state]
+  end
+
   def button_down(id)
     close if id == Gosu::KbEscape
-    @states[@current_state].button_down(id)
+    current_state.button_down(id)
   end
 
   def draw
     # Forward drawing to the current gamestate.
-    @states[@current_state].draw
+    current_state.draw
   end
 
   def update
     if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft
-      @states[@current_state].left
+      current_state.left
     end
     if button_down? Gosu::KbRight or button_down? Gosu::GpRight
-      @states[@current_state].right
+      current_state.right
     end
     if button_down? Gosu::KbUp or button_down? Gosu::GpUp
-      @states[@current_state].up
+      current_state.up
     end
     if button_down? Gosu::KbDown or button_down? Gosu::GpDown
-      @states[@current_state].down
+      current_state.down
     end
     if button_down? Gosu::KbSpace or button_down? Gosu::GpButton0
-      @states[@current_state].space
+      current_state.space
     end
 
     unless button_down? Gosu::KbUp or button_down? Gosu::GpUp or button_down? Gosu::KbDown or button_down? Gosu::GpDown
-      @states[@current_state].rest
+      current_state.rest
     end
 
     # Forward updating to the current gamestate.
-    @states[@current_state].update
+    current_state.update
   end
 end
 
